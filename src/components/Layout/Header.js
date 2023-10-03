c84901f6bafb6877b646694b5925e70bf8d7a8fc
ridwan-1989 <ridwanhossain@bluedrop.com>
2023-10-03 16:28:33 -0230

    test remote

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+const Header = props => {
+	return <Fragment>
+		<header>
+			<h1>Food Order App</h1>
+			<button>Cart</button>
+		</header>
+		<div></div>
+	</Fragment>
+};
+
+export default Header;
