696c567d9933e07a3ed84b6ceae1104ea241179b
rbh546 <rbh546@mun.ca>
2023-11-25 21:03:12 -0330

    add cart section

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,18 @@
 import { Fragment } from "react";
 
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
 const Header = props => {
 	return <Fragment>
-		<header>
-			<h1>Food Order App</h1>
-			<button>Cart</button>
+		<header className={classes.header}>
+			<h1>Food Order App test</h1>
+			<HeaderCartButton />
 		</header>
-		<div></div>
+		<div className={classes['main-image']}>
+			<img src={mealsImage} alt= 'Table with foods'/>
+		</div>
 	</Fragment>
 };
 
